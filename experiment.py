614450aee1dede1defdd020d9043e8f705a5b7f4
Shourya <mittakantishourya@gmail.com>
2026-07-19 11:16:44 -0500

    the v-2

---
+++ experiment.py
@@ -0,0 +1,39 @@
+# Basic Python code with 10 variables
+
+# 1. Integer
+age = 25
+
+# 2. Float
+height = 5.9
+
+# 3. String
+name = "Shourya"
+
+# 4. Boolean
+is_student = True
+
+# 5. List
+scores = [85, 90, 78, 92]
+
+# 6. Dictionary
+person = {"name": "Shourya", "city": "Overland Park"}
+
+# 7. Tuple
+coordinates = (38.99, -94.67)
+
+# 8. Set
+unique_numbers = {1, 2, 3, 3, 2}
+
+# 9. NoneType
+middle_name = None
+
+# 10. Complex number
+complex_num = 4 + 5j
+
+print(name, age, height, is_student)
+print(scores)
+print(person)
+print(coordinates)
+print(unique_numbers)
+print(middle_name)
+print(complex_num)
